467a86cc9c89eed324210cc0aa2a6787121d0640
AhmedHB <ahmed.haji.bile@gmail.com>
2023-03-17 17:10:28 +0100

    Added calculation endpoint

---
+++ src/main/java/com/ahmed/Calculator/CalculatorController.java
@@ -0,0 +1,22 @@
+package com.ahmed.Calculator;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorController {
+    @Autowired
+    private Calculator calculator;
+
+    @GetMapping(
+            value = "/calculate-sum",
+            produces = "application/json; charset=utf-8")
+    public String sum(@RequestParam Integer a,
+                      @RequestParam Integer b){
+        return String.valueOf(calculator.sum(a, b));
+    }
+}
--- src/test/java/com/ahmed/Calculator/CalculatorApplicationTests.java
+++ src/test/java/com/ahmed/Calculator/CalculatorApplicationTests.java
@@ -1,13 +1,48 @@
 package com.ahmed.Calculator;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 class CalculatorApplicationTests {
+	@Autowired
+	private WebTestClient client;
+
+	private Calculator calculator = new Calculator();
 
 	@Test
 	void contextLoads() {
+	}
+	@Test
+	public void testSum(){
+		assertEquals(5, calculator.sum(2,3));
+	}
+
+	private static final int SUM_OK = 5;
+
+	@Test
+	void getOKSum() {
+		getAndVerifySum(3,2, OK)
+				.returnResult().getResponseBody().equals(SUM_OK);
+	}
+
+	private WebTestClient.BodyContentSpec getAndVerifySum(Integer a, Integer b, HttpStatus expectedStatus) {
+		return client.get()
+				.uri("/calculate-sum?a=" + a + "&b="+  b)
+				.accept(MediaType.valueOf("application/json; charset=utf-8"))
+				.exchange()
+				.expectStatus().isEqualTo(expectedStatus)
+				.expectHeader().contentType("application/json; charset=utf-8")
+				.expectBody();
 	}
 
 }
